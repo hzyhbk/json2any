e9247d53e7dbdd4747eb7cbd2bcf8d071233c583
hzyhbk <hzyhbk@163.com>
2023-03-22 10:37:56 +0800

    fix: 修复 css 转化问题

--- src/json2css.ts
+++ src/json2css.ts
@@ -1,18 +1,20 @@
 export function toCSS(str: string) {
-  const cssArray = str.split(',');
+  const cssArray = str.split('\n');
   const newCssArray = cssArray.map((item) => {
     if (item) {
       let [name, value] = item.split(':');
 
-      name = name.replace(/[A-Z]/g, function (match) {
+      name = name.trim().replace(/[A-Z]/g, function (match) {
         return '-' + match.toLowerCase();
       });
-
+      // 单引号去掉
       value = value.replace(/'/g, '');
+      // 结尾逗号去掉
+      value = value.replace(/,$/, '');
 
       return `${name}:${value}`;
     }
     return item;
   });
-  return newCssArray.join(';');
+  return newCssArray.join(';\n');
 }
